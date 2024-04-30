20f1c044f5cdad805958cc9be279f0011a3ddbe5
sgsm74 <saeed.razii93@gmail.com>
2024-05-01 00:37:33 +0330

    chore: add some icon

--- lib/core/consts/assets.dart
+++ lib/core/consts/assets.dart
@@ -2,3 +2,13 @@ class ImageAssets {
   static const _path = 'assets/images/';
   static const String logo = '${_path}logo.png';
 }
+
+class IconAssets {
+  static const _path = 'assets/icons/';
+  static const String settings = '${_path}settings.png';
+  static const String timesheets = '${_path}clock.png';
+  static const String projects = '${_path}case.png';
+  static const String odoo = '${_path}odoo.png';
+  static const String timesheets1 = '${_path}timesheets.png';
+  static const String favorites = '${_path}favorites.png';
+}
